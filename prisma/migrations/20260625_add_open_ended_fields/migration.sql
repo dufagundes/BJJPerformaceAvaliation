30ca3b8a4ac4d4cac0f7720f5f7ede2a331196eb
Eduardo Ferreira <dufagundes@gmail.com>
2026-06-25 14:25:31 -0500

    Make open-ended feedback migration idempotent

--- prisma/migrations/20260625_add_open_ended_fields/migration.sql
+++ prisma/migrations/20260625_add_open_ended_fields/migration.sql
@@ -1,3 +1,3 @@
 -- Add open-ended response fields to EvaluationResponse table
-ALTER TABLE "EvaluationResponse" ADD COLUMN "strengths_text" TEXT;
-ALTER TABLE "EvaluationResponse" ADD COLUMN "improvements_text" TEXT;
+ALTER TABLE "EvaluationResponse" ADD COLUMN IF NOT EXISTS "strengths_text" TEXT;
+ALTER TABLE "EvaluationResponse" ADD COLUMN IF NOT EXISTS "improvements_text" TEXT;
